2cf7f4cdbeb74df8f535caec5e832d7509587cc4
Semih Özdamar <semih.oezdamar@stud.tu-darmstadt.de>
2023-03-14 10:35:41 +0100

    state and category loop added

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,29 +1,28 @@
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { catgoryId: 1, catgoryName: "Beverages" },
+        { catgoryId: 2, catgoryName: "Condiments" },
+      ],
+    };
+  }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem action active href="#" tag="a">
-            Cras justo odio
-          </ListGroupItem>
-          <ListGroupItem action href="#" tag="a">
-            Dapibus ac facilisis in
-          </ListGroupItem>
-          <ListGroupItem action href="#" tag="a">
-            Morbi leo risus
-          </ListGroupItem>
-          <ListGroupItem action href="#" tag="a">
-            Porta ac consectetur ac
-          </ListGroupItem>
-          <ListGroupItem action disabled href="#" tag="a">
-            Vestibulum at eros
-          </ListGroupItem>
+          {this.state.categories.map((catgory) => (
+            <ListGroupItem key={catgory.catgoryId}>
+              {catgory.catgoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
-        <p />
       </div>
     );
   }
