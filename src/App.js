4801972523296b76d98d93ee222e2a91e3fe02ae
Semih Özdamar <semih.oezdamar@stud.tu-darmstadt.de>
2023-03-14 09:31:42 +0100

    react-strap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import CategoryList from "./CategoryList";
 import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Col, Container, Row } from "reactstrap";
 
 function App() {
   return (
     <div>
-      <h3>Hello from react!!!</h3>
-      <Navi></Navi>
+      {/* <h3>Hello from react!!!</h3> */}
+
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
